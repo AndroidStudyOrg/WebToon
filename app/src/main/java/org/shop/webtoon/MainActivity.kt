055bd9df1e6f5749fbb3ea2b447ad3301fa01ccd
wbham <wbham97@naver.com>
2023-12-25 01:42:51 +0900

    [Feat] - ViewPager와 TabLayout 적용
    - ViewPager2 어댑터로 RecyclerView를 상속받은 Adapter가 아닌, FragmentStateAdapter라는 것이 있음. 이를 통해 간단하게 설정 가능
    - MainActivity에서 ViewPager의 어댑터 연결 후 TabLayoutMediator를 이용해서 Tab과 ViewPager를 연동

--- app/src/main/java/org/shop/webtoon/MainActivity.kt
+++ app/src/main/java/org/shop/webtoon/MainActivity.kt
@@ -2,7 +2,10 @@ package org.shop.webtoon
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import org.shop.webtoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,31 +16,30 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
 
-        binding.button1.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, WebViewFragment())
-                commit()
-            }
-        }
+        binding.viewPager.adapter = ViewPagerAdapter(this)
 
-        binding.button2.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, BFragment())
-                commit()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            run {
+                val textView = TextView(this@MainActivity)
+                textView.text = "position $position"
+                textView.gravity = Gravity.CENTER
+
+                tab.customView = textView
+//                tab.text = "position $position"
             }
-        }
+        }.attach()
     }
 
     override fun onBackPressed() {
-        val currentFragment = supportFragmentManager.fragments[0]
+        val currentFragment = supportFragmentManager.fragments[binding.viewPager.currentItem]
         Log.d("MainActivity BackPressed", currentFragment.toString())
-        if(currentFragment is WebViewFragment){
-            if (currentFragment.canGoBack()){
+        if (currentFragment is WebViewFragment) {
+            if (currentFragment.canGoBack()) {
                 currentFragment.goBack()
-            }else{
+            } else {
                 super.onBackPressed()
             }
-        }else{
+        } else {
             super.onBackPressed()
         }
     }
