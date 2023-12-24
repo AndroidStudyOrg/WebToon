fbfd0afea0d7ab39021facf86933e41d9ec99d31
wbham <wbham97@naver.com>
2023-12-24 23:49:25 +0900

    [Feat] - WebView 띄우기

--- app/src/main/java/org/shop/webtoon/MainActivity.kt
+++ app/src/main/java/org/shop/webtoon/MainActivity.kt
@@ -1,11 +1,18 @@
 package org.shop.webtoon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com")
     }
 }
